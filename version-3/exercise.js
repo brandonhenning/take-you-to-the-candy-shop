9e34e8e8364760f558002c834a247dce1fb82071
Brandon Henning <brandonhenning@Brandons-MacBook-Pro-2.local>
2018-03-10 11:52:16 -0700

    added drill 3 js file

--- version-3/exercise.js
+++ version-3/exercise.js
@@ -1,6 +1,34 @@
 module.exports = {
     // Uncomment these functions as you write them
-    // getTotalItemSales,
-    // addTotalSalesToItems,
-    // addTotalValueToItems
+    getTotalItemSales,
+    addTotalSalesToItems,
+    addTotalValueToItems
 }
+
+
+function getTotalItemSales (list, id) {
+    return list.filter((element) => {
+        return element.itemId === (id)
+    }).reduce((total, element) => {
+        total += element.quantity
+        return total
+    }, 0)
+}
+
+
+function addTotalSalesToItems (salesList, itemsList) {
+    return itemsList.map((element) => {
+        element.quantity = getTotalItemSales(salesList, element.id)
+        return element
+    })
+}
+
+
+function addTotalValueToItems (salesList, itemsList) {
+    return addTotalSalesToItems(salesList, itemsList).map((element) => {
+        element.totalValue = (element.quantity * element.price).toFixed(2)
+        return element
+    })
+}
+
+
