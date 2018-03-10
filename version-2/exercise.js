131cd1ff7a516d07b050e05c96408adf7c62cd73
Brandon Henning <brandonhenning@Brandons-MacBook-Pro-2.local>
2018-03-10 11:05:04 -0700

    added drill 2 js file

--- version-2/exercise.js
+++ version-2/exercise.js
@@ -1,6 +1,33 @@
 module.exports = {
     // Uncomment these as you write them
-    // getItemById,
-    // buildTransactions,
-    // getTransactionsByItemDescription
+    getItemById,
+    buildTransactions,
+    getTransactionsByItemDescription
 }
+
+
+function getItemById (list, itemId) {
+    answerArr = list.filter(function (element) {
+        return element.id === itemId;
+    })
+    return answerArr[0]
+}
+
+
+function buildTransactions (salesList, itemsList) {
+    return salesList.map(function (element) {
+        return Object.assign(element, getItemById(itemsList, element.itemId))
+    }).map(function (element) {
+        delete element.id
+        return element
+    })
+}
+
+
+function getTransactionsByItemDescription (list, description) {
+    return list.filter(function (element) {
+        return element.description == description
+    })
+}
+
+
